9641d2e3e7fe94326ec0045a666877149fa33b0f
vhkpawan <vhkpawan@gmail.com>
2025-09-21 20:22:36 -0400

    initial commit

---
+++ src/main/java/com/p23studios/basicspringangularapi/BasicSpringAngularApiApplication.java
@@ -0,0 +1,13 @@
+package com.p23studios.basicspringangularapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringAngularApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSpringAngularApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/p23studios/basicspringangularapi/BasicSpringAngularApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p23studios.basicspringangularapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringAngularApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
